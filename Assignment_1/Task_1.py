b5d128d724b84038328886d8f1b726c2b2a22c1e
Rhythm-1987 <rhythmbhojani07@gmail.com>
2026-01-06 17:15:25 +0530

    Assignment_1 Upload

---
+++ Assignment_1/Task_1.py
@@ -0,0 +1,15 @@
+a = input("Enter a number: ")
+b = input("Enter another number: ")
+
+a = int(a)
+b = int(b)
+
+add = a + b
+sub = a - b
+mul = a * b
+div = a / b
+
+print("Addition: ", add)
+print("Subtraction: ", sub)
+print("Multiplication: ", mul)
+print("Division: ", div)
---
+++ Assignment_1/Task_2.py
@@ -0,0 +1,8 @@
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+s1 = "Hello, "
+s2 = first_name + " " + last_name
+s3 = "! Welcome to the Python program."
+
+print(s1 + s2 + s3)
