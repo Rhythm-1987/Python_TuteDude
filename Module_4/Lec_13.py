46fd713083e78b60eb33587c5fa2d2c07fd1f6e0
Rhythm-1987 <rhythmbhojani07@gmail.com>
2026-01-06 15:43:37 +0530

    trial

---
+++ Module_4/Lec_13.py
@@ -0,0 +1,10 @@
+"""
+print("Hello Everyone! We are learning Python.")
+num = 10
+print(num)
+"""
+
+name = input("Please enter your name: ")
+age = int(input("Please enter your age: "))
+
+print(name, age)
---
+++ Module_4/Lec_14.py
@@ -0,0 +1,4 @@
+print(max(1, 2,2,3,4,5,8))
+print(min(10,19,8,2,46))
+print(abs(-2020))
+print(pow(2,3))
