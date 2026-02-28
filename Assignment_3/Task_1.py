96139ab9d36029eab9602430e2a0990e21719713
Rhythm-1987 <rhythmbhojani07@gmail.com>
2026-02-28 14:32:34 +0530

    Added factorial using recursion program

--- Assignment_3/Task_1.py
+++ Assignment_3/Task_1.py
@@ -1,9 +1,15 @@
-def factorial(number):
-    answer = 1
-    while number > 1:
-        answer = answer * number
-        number = number - 1
-    return answer
+# Function to calculate factorial using recursion
+def fact(number):
+    # Base case: factorial of 0 and 1 is 1, recursion stops here
+    if number == 1:
+        return 1
+    # Recursive case: n! = n * (n-1)!
+    else:
+        factorial = number * fact(number - 1)
+        return factorial
 
 num = int(input("Enter a number: "))
-print(f"Factorial of {num} is: {factorial(num)}")
+if num <= 0:
+    print("The number entered must not be a negative value or zero.\nPlease rerun the program.")
+else:
+    print(f"Factorial of {num} is: {fact(num)}")
--- Assignment_3/Task_2.py
+++ Assignment_3/Task_2.py
@@ -2,6 +2,10 @@ import math
 
 number = int(input("Enter a number: "))
 
-print(f"Square root: {math.sqrt(number)}")
-print(f"Logarithm: {math.log(number, math.e)}")
+if number<= 0:
+    print("Enter a positive value for the square root and logarithm")
+else:
+    print(f"Square root: {math.sqrt(number)}")
+    print(f"Logarithm: {math.log(number, math.e)}")
+
 print(f"Sine: {math.sin(number)}")
